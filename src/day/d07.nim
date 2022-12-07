cd5ed3e21de1f27457b3255d62e51099e69b1ee7
Bob Geis <bobgeis@users.noreply.github.com>
2022-12-07 13:39:13 -0500

    d07 use foldl

--- src/day/d07.nim
+++ src/day/d07.nim
@@ -3,7 +3,10 @@ include ../lib/imps
 
 day 7:
 
-  # In practice, the input calls ls in a dir, then cds into each dir inside it in turn, recursively walking the whole tree and calling ls just once in each dir.
+  part 0: """The input recursively walks the dirs,
+    calling `ls` exactly once in each.
+    It could have been much worse!"""
+
   var
     curr = ""
     sizes = initCountTable[string]()
@@ -21,24 +24,14 @@ day 7:
     elif line == "$ cd ..": popdir()
     elif line.scanf("$$ cd $+",name): pushdir(name)
     elif line.scanf("$i $+",size,name): sizes.inc curr,size
-    else: discard
   while curr != "": popdir()
 
-  var total = 0
-  for size in sizes.values:
-    if size < 100_000:
-      total += size
-
-  part 1: total
+  part 1: sizes.values.toseq.foldl(if b < 100_000: a + b else: a,0)
   answer 1: 1_908_462
   answer 1, "t1": 95_437
 
   let bound = sizes[""] - 40_000_000
-  var smallest = int.high
-  for size in sizes.values:
-    if size <= smallest and size >= bound:
-      smallest = size
 
-  part 2: smallest
+  part 2: sizes.values.toseq.foldl(if b <= a and b >= bound: b else: a, int.high)
   answer 2: 3_979_145
   answer 2, "t1": 24_933_642
