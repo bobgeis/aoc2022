08d6fca084e2e6ffd59d6aea6d47330349661df5
Bob Geis <bobgeis@users.noreply.github.com>
2022-12-11 11:16:56 -0500

    d11 cleanup part 1, are enums cleaner than closures?

--- src/day/d11.nim
+++ src/day/d11.nim
@@ -4,43 +4,42 @@ include ../lib/imps
 day 11:
   let input = path.readFile.split("\n\n")
 
-  type Monkey = object
-    id: int
-    items: seq[int]
-    op: (int) -> int
-    divtest: int
-    iftrue,iffalse: int
-    count: int
+  type
+    Opk = enum oAd, oMu, oSq
+    Monkey = object
+      id: int
+      items: seq[int]
+      op: Opk
+      opo: int
+      divtest: int
+      iftrue,iffalse: int
+      count: int
 
-  proc getop(s:string):(int) -> int =
-    var i:int
-    if s.scanf("  Operation: new = old + $i",i):
-      return proc (j:int):int = j + i
-    elif s.scanf("  Operation: new = old * $i",i):
-      return proc (j:int):int = j * i
-    elif s.scanf("  Operation: new = old * old",i):
-      return proc (j:int):int = j * j
-    else: raise
+  proc doop(m:Monkey,w:int):int =
+    case m.op:
+      of oAd: m.opo + w
+      of oMu: m.opo * w
+      of oSq: w * w
 
   proc parseMonkey(ss:seq[string]):Monkey =
-    let (_,id) = ss[0].scanTuple("Monkey $i:")
-    let items = ss[1].parseInts
-    let op = ss[2].getop
-    let (_,divtest) = ss[3].scanTuple("  Test: divisible by $i")
-    let (_,iftrue) = ss[4].scanTuple("    If true: throw to monkey $i")
-    let (_,iffalse) = ss[5].scanTuple("    If false: throw to monkey $i")
-    Monkey(
-      id:id,items:items,op:op,divtest:divtest,
-      iftrue:iftrue,iffalse:iffalse,
-      count:0
-    )
+    result.items = ss[1].parseInts
+    if ss[2].scanf("  Operation: new = old + $i",result.opo):
+      result.op = oAd
+    elif ss[2].scanf("  Operation: new = old * $i",result.opo):
+      result.op = oMu
+    elif ss[2].scanf("  Operation: new = old * old"):
+      result.op = oSq
+    doAssert ss[3].scanf("  Test: divisible by $i",result.divtest)
+    doAssert ss[4].scanf("    If true: throw to monkey $i",result.iftrue)
+    doAssert ss[5].scanf("    If false: throw to monkey $i",result.iffalse)
 
-  var ms = input.mapit(it.splitlines.parseMonkey)
+  let monkeys = input.mapit(it.splitlines.parseMonkey)
+  var ms = monkeys
 
   proc act(m:var Monkey) =
     m.count += m.items.len
     for i in m.items:
-      let w = m.op(i) div 3
+      let w = m.doop(i) div 3
       if w mod m.divtest == 0:
         ms[m.iftrue].items.add w
       else:
