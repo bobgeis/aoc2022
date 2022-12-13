24133eea11840fa5e9b63bc75699a6436f9ad4af
Bob Geis <bobgeis@users.noreply.github.com>
2022-12-12 21:19:04 -0500

    d12 solve part 2

--- src/day/d12.nim
+++ src/day/d12.nim
@@ -22,12 +22,23 @@ day 12:
   proc adjs(v:Vec2i):seq[Vec2i] =
     let h = map[v]
     for u in v.getAdj4:
-      if u in map and map[u] <= h + 1: result.add u
-
-  proc fin(paths:Table[Vec2i,(int,Vec2i)], v:Vec2i):bool =
-    v == finish
+      if u in map and map[u] >= h - 1: result.add u
 
   part 1:
-    bfs(start,adjs,fin)[finish][0]
+    proc fin(paths:Table[Vec2i,(int,Vec2i)], v:Vec2i):bool =
+      v == start
+    bfs(finish,adjs,fin)[start][0]
   answer 1: 534
   answer 1, "t1": 31
+
+  part 2:
+    var dist:int
+    proc fin(paths:Table[Vec2i,(int,Vec2i)],v:Vec2i):bool =
+      if map[v] == 0:
+        dist = paths[v][0]
+        return true
+      else: return false
+    discard bfs(finish,adjs,fin)
+    dist
+  answer 2: 525
+  answer 2, "t1": 29
