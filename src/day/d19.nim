8e55fb8f78f2abe00d085fcb5fb4914c2641f8da
Bob Geis <bobgeis@users.noreply.github.com>
2022-12-19 06:39:50 -0500

    d19 init

---
+++ src/day/d19.nim
@@ -0,0 +1,15 @@
+## https://adventofcode.com/2022/day/19
+include ../lib/imps
+
+day 19:
+  prep:
+# Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 4 ore. Each obsidian robot costs 4 ore and 18 clay. Each geode robot costs 4 ore and 9 obsidian.
+
+    let input = path.readFile
+
+
+  part 1:
+    expectT 33
+    # expect 0
+    "?"
+
